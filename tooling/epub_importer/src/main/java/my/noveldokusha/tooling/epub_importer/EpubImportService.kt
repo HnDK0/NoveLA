434c9140d8ce8072a77cf4e007fca720a5a3f54c
Vaizer0 <137601862+Vaizer0@users.noreply.github.com>
2026-07-13 11:09:21 +0530

    Remove PDFBox dependency from local PDF import

--- tooling/epub_importer/src/main/java/my/noveldokusha/tooling/epub_importer/EpubImportService.kt
+++ tooling/epub_importer/src/main/java/my/noveldokusha/tooling/epub_importer/EpubImportService.kt
@@ -22,7 +22,6 @@ import my.noveldokusha.core.asSequence
 import my.noveldokusha.core.tryAsResponse
 import my.noveldokusha.core.utils.Extra_Uri
 import my.noveldokusha.core.utils.isServiceRunning
-import my.noveldokusha.epub_tooling.epubParser
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -37,17 +36,21 @@ class EpubImportService : Service() {
 
     private class IntentData : Intent {
         var uri by Extra_Uri()
+        var importType: String?
+            get() = getStringExtra("importType")
+            set(value) { putExtra("importType", value) }
 
         constructor(intent: Intent) : super(intent)
-        constructor(ctx: Context, uri: Uri) : super(ctx, EpubImportService::class.java) {
+        constructor(ctx: Context, uri: Uri, type: LocalBookImportType) : super(ctx, EpubImportService::class.java) {
             this.uri = uri
+            this.importType = type.name
         }
     }
 
     companion object {
-        fun start(ctx: Context, uri: Uri) {
+        fun start(ctx: Context, uri: Uri, type: LocalBookImportType = LocalBookImportType.EPUB) {
             if (!isRunning(ctx))
-                ContextCompat.startForegroundService(ctx, IntentData(ctx, uri))
+                ContextCompat.startForegroundService(ctx, IntentData(ctx, uri, type))
         }
 
         private fun isRunning(context: Context): Boolean =
@@ -87,23 +90,10 @@ class EpubImportService : Service() {
                     notificationBuilder,
                     notificationId = notificationId
                 ) {
-                    title = getString(R.string.import_epub)
+                    title = getString(R.string.importing_local_book)
                     foregroundServiceBehavior = NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE
                     setProgress(100, 0, true)
                 }
-                val inputStream = contentResolver.openInputStream(intentData.uri)
-                if (inputStream == null) {
-                    notificationsCenter.showNotification(
-                        channelName = channelName,
-                        channelId = channelId,
-                        notificationId = "Import EPUB failure".hashCode()
-                    ) {
-                        text = getString(R.string.failed_get_file)
-                        removeProgressBar()
-                    }
-                    return@tryAsResponse
-                }
-
                 val fileName = contentResolver.query(
                     intentData.uri,
                     arrayOf(OpenableColumns.DISPLAY_NAME),
@@ -113,16 +103,24 @@ class EpubImportService : Service() {
                     null
                 ).asSequence().map { it.getString(0) }.last()
 
-                val epub = inputStream.use { epubParser(inputStream = it) }
+                val importType = intentData.importType
+                    ?.let { LocalBookImportType.valueOf(it) }
+                    ?: LocalBookImportType.EPUB
+                val epub = parseLocalBook(
+                    context = this@EpubImportService,
+                    uri = intentData.uri,
+                    fileName = fileName,
+                    type = importType
+                )
 
                 notificationsCenter.modifyNotification(
                     notificationBuilder,
                     notificationId = notificationId
                 ) {
-                    text = getString(R.string.importing_epub)
+                    text = getString(R.string.importing_local_book)
                 }
                 epubImporterRepository.epubImporter(
-                    storageFolderName = fileName,
+                    storageFolderName = if (importType == LocalBookImportType.EPUB) fileName else epub.title.ifBlank { fileName },
                     epub = epub,
                     addToLibrary = true
                 )
@@ -133,7 +131,7 @@ class EpubImportService : Service() {
                     channelId = channelId,
                     notificationId = "Import EPUB failure".hashCode()
                 ) {
-                    text = getString(R.string.failed_to_import_epub)
+                    text = getString(R.string.failed_to_import_local_book)
                     setSubText(it.message)
                     removeProgressBar()
                 }
