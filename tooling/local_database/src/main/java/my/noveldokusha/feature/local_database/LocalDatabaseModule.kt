c8fcd676518685b52d25ed560482054dc76ef756
Vaibhav Hegadkar <137601862+Vaizer0@users.noreply.github.com>
2026-08-09 17:41:17 +0530

    fix: provide ChapterPagesDao in Hilt LocalDatabaseModule

--- tooling/local_database/src/main/java/my/noveldokusha/feature/local_database/LocalDatabaseModule.kt
+++ tooling/local_database/src/main/java/my/noveldokusha/feature/local_database/LocalDatabaseModule.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import my.noveldokusha.feature.local_database.DAOs.ChapterBodyDao
 import my.noveldokusha.feature.local_database.DAOs.ChapterDao
+import my.noveldokusha.feature.local_database.DAOs.ChapterPagesDao
 import my.noveldokusha.feature.local_database.DAOs.ChapterTranslationDao
 import my.noveldokusha.feature.local_database.DAOs.DownloadTaskDao
 import my.noveldokusha.feature.local_database.DAOs.LibraryDao
@@ -41,6 +42,10 @@ abstract class LocalDatabaseModule {
         @Provides
         @Singleton
         fun provideChapterBodyDao(database: AppDatabase): ChapterBodyDao = database.chapterBodyDao()
+
+        @Provides
+        @Singleton
+        fun provideChapterPagesDao(database: AppDatabase): ChapterPagesDao = database.chapterPagesDao()
 
         @Provides
         @Singleton
