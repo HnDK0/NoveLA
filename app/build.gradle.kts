e11f92968dd43e8a3a3034bf69655c4b731cd765
HnDK0 <212071365+HnDK0@users.noreply.github.com>
2026-08-22 04:57:34 +0300

    Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
 
     defaultConfig {
         applicationId = "my.novela"
-        versionCode = 36
-        versionName = "1.5.0"
+        versionCode = 37
+        versionName = "1.5.1"
         base.archivesName.set("NoveLA_v$versionName")
         manifestPlaceholders["appLabel"] = "NoveLA"
     }
